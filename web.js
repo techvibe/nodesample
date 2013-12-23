b20dc6c405bab1ae88e091d978de97cfa68dc09f
techvibe <sreebodapati@gmail.com>
2013-12-23 19:49:09 +0000

    soo-on hello

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello Sreenivas!');
+  response.send('Hello Soo-on!');
 });
 
 var port = process.env.PORT || 5000;
